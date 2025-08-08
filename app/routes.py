541024cb285bb0acd59210d8e7a131d8b395f178
marllonDev <marlonzucolotto@gmail.com>
2025-08-08 12:35:07 -0300

    Adicionando endpoint GET no routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,14 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, json
 from . import producer  # Importa o nosso módulo producer
 from . import config    # Importa o nosso módulo de configuração
+from . import database  # Importa o nosso módulo de banco de dados
+
 
 # Aqui criamos a instância principal da nossa aplicação Flask.
 # Mais tarde, nosso script 'run_api.py' irá importar esta variável 'app'.
 app: Flask = Flask(__name__)
 
-# --- Definição do Endpoint da API ---
+# --- Definição do Endpoint POST da API ---
 @app.route('/sugestao', methods=['POST'])
 def rota_enviar_sugestao():
     """
@@ -32,5 +34,27 @@ def rota_enviar_sugestao():
         # Se o produtor não conseguiu enviar, retornamos um erro de serviço indisponível.
         return jsonify({"status": "erro", "mensagem": "Serviço de sugestões indisponível no momento."}), 503
 
-# No futuro, se quisermos criar o endpoint GET /sugestoes,
-# nós o adicionaríamos aqui neste mesmo arquivo.
+
+# --- Endpoint GET ---
+@app.route('/sugestoes', methods=['GET'])
+def rota_get_sugestoes():
+    """
+    Busca todas as sugestões no banco de dados e as retorna como JSON.
+    """
+    print("Recebida requisição para buscar todas as sugestões.")
+    
+    # Simplesmente chamamos a função do nosso especialista em banco de dados.
+    sugestoes= database.get_all_sugestoes()
+    
+    # O psycopg2 retorna objetos de data/hora que não são diretamente
+    # serializáveis para JSON. A função json.dumps com o parâmetro 'default=str'
+    # resolve isso, convertendo esses objetos em strings.
+    # O Flask.jsonify não tem essa opção, então fazemos a conversão em duas etapas.
+    response_data: str = json.dumps(sugestoes, default=str)
+    
+    # Criamos uma resposta Flask a partir do JSON formatado.
+    return app.response_class(
+        response=response_data,
+        status=200,
+        mimetype='application/json'
+    )
